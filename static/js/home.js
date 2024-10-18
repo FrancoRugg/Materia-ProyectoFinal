2bbe003bf9f1f29de0f4278643a41a533757383e
FrancoRugg <francomarceloruggiero@gmail.com>
2024-10-18 18:18:40 -0300

    Products CSS

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     // openLoad();
     getSections()
-
+    getOptionsFromSections();
 }
 document.addEventListener('DOMContentLoaded', () => {
     // closeLoad();
@@ -50,9 +50,14 @@ async function getSections() {
         let sectionsHTML = "";
 
         sectionsHTML += `
-        <div class="${visible}" id="add_section">
-            <i class='bx bx-list-plus' ></i>
+        <article class="new_buttons">
+        <div class="${visible}" id="add_product">
+            <i class='bx bxl-product-hunt'></i>
         </div>
+            <div class="${visible}" id="add_section">
+                <i class='bx bx-list-plus' ></i>
+            </div>
+        </article>
         `;
         for (const e of res) {
             const s_name = e.name;
@@ -77,6 +82,8 @@ async function getSections() {
                     for (const p of products) {
                         const p_id = p.id;
                         const p_name = p.name;
+                        const p_section = s_name;
+                        const p_id_section = s_id;
                         const p_price = p.price;
                         const p_active = p.active;
 
@@ -84,11 +91,11 @@ async function getSections() {
                             productsHTML += `
                                 <article class="Product">
                                     <div>
-                                        <div class="p_edit ${visible}" data-id="${p_id}">
+                                        <div class="p_edit ${visible}" data-id="${p_id}" data-p_name="${p_name}" data-p_id_section="${p_id_section}" data-p_section="${p_section}" data-p_price="${p_price}" data-p_active="${p_active}">
                                             <i class='bx bxs-edit'></i>
                                         </div>
                                         <p class="p_name">${p_name}</p>
-                                        <p class="p_price">$ ${p_price}</p>
+                                        <p class="p_price">${p_price}</p>
                                     </div>
                                     <div>
                                         <button type="button" id="${p_id}">Add</button>
@@ -106,6 +113,7 @@ async function getSections() {
         }
 
         data_home.innerHTML = sectionsHTML;
+        openAndCloseProduct();
         openAndCloseSection();
         closeLoad()
     } catch (error) {
@@ -204,7 +212,147 @@ function openAndCloseSection() {
         })
     }
 }
+function openAndCloseProduct() {
 
+    //Cierra la ventana de Productos
+    const close_product = document.getElementById('close_product');
+    if (close_product) {
+        close_product.addEventListener('click', () => {
+            const see_product = document.getElementById('see_product');
+            see_product.classList.toggle('on');
+            see_product.classList.add('off');
+
+            const send_product = document.getElementById('send_product');
+            send_product.classList.remove('on');
+            send_product.classList.add('off');
+
+            const edit_product = document.getElementById('edit_product');
+            edit_product.classList.remove('on');
+            edit_product.classList.add('off');
+
+            const see_id = document.getElementById('see_p_id');
+            see_id.classList.remove('on');
+            see_id.classList.add('off');
+
+            const see_p_active = document.getElementById('see_p_active');
+            see_p_active.classList.add('off');
+            see_p_active.classList.remove('on');
+
+            const get_id = document.getElementById('get_p_id');
+            get_id.value = "";
+            const set_product = document.getElementById('set_product');
+            set_product.value = "";
+            const set_price = document.getElementById('set_price');
+            set_price.value = "";
+
+            // let p_section = document.querySelectorAll('#set_p_section option');
+            // if (p_section) {
+            //     p_section.forEach((elem) => {
+            //         console.log(elem);
+            //         // elem.selected = false;
+            //         if (elem.value == 0) {
+            //             elem.setAttribute('selected', 'selected');
+            //             // elem.selected = true;
+            //         } else {
+            //             elem.removeAttribute('selected');
+            //         }
+            //     })
+            // }
+            getOptionsFromSections();
+        })
+    }
+    //Abre ventana de Productos
+    const add_product = document.getElementById('add_product');
+    if (add_product) {
+        add_product.addEventListener('click', () => {
+            // console.log('click')
+            const see_product = document.getElementById('see_product');
+            see_product.classList.toggle('off');
+            see_product.classList.add('on');
+
+            const send_product = document.getElementById('send_product');
+            send_product.classList.remove('off');
+            send_product.classList.add('on');
+
+        })
+    }
+    //Abre ventana de editar Productos
+    const p_edit = document.querySelectorAll('.p_edit');
+    if (p_edit.length > 0) {
+        p_edit.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                // console.log(elem.dataset)
+                // console.log(elem.dataset.id)
+                const see_product = document.getElementById('see_product');
+                see_product.classList.toggle('off');
+                see_product.classList.add('on');
+
+                const edit_product = document.getElementById('edit_product');
+                edit_product.classList.remove('off');
+                edit_product.classList.add('on');
+
+                const see_id = document.getElementById('see_p_id');
+                see_id.classList.remove('off');
+                see_id.classList.add('on');
+
+                const see_active = document.getElementById('see_p_active');
+                see_active.classList.remove('off');
+                see_active.classList.add('on');
+
+                const get_id = document.getElementById('get_p_id');
+                get_id.value = `${elem.dataset.id}`;
+                const set_product = document.getElementById('set_product');
+                set_product.value = `${elem.dataset.p_name}`;
+                const set_price = document.getElementById('set_price');
+                set_price.value = `${elem.dataset.p_price}`;
+                let active = elem.dataset.p_active;
+                let allActives = document.querySelectorAll('#set_p_active option');
+                if (allActives) {
+                    allActives.forEach((elem) => {
+                        // console.log(elem);
+                        // elem.selected = false;
+                        if (elem.value == active) {
+                            elem.setAttribute('selected', 'selected');
+                            // elem.selected = true;
+                        }
+                    })
+                }
+                // const set_p_section = document.getElementById('set_p_section');
+                let p_section = document.querySelectorAll('#set_p_section option');
+                let p_sec_name = elem.dataset.p_id_section;
+                if (p_section) {
+                    p_section.forEach((elem) => {
+                        // console.log(elem);
+                        // elem.selected = false;
+                        if (elem.value == p_sec_name) {
+                            elem.setAttribute('selected', 'selected');
+                            // elem.selected = true;
+                        }
+                    })
+                }
+            })
+        })
+    }
+}
+
+async function getOptionsFromSections() {
+    try {
+        const response = await getFetch('/getSectorData', 'GET');
+        const res = JSON.parse(response);
+
+        const set_p_section = document.getElementById('set_p_section');
+        let options = "";
+        options += `<option value="0" selected disabled>Elija una opción ...</option>`;
+        res.forEach((elem) => {
+            // console.log(elem);
+            options += `<option value="${elem.id}">${elem.name}</option>`;
+        })
+        set_p_section.innerHTML = options;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
 
 function getProducts(sectorId) {
     getFetch(`/getProducts?sectorId=${sectorId}`, 'GET')
